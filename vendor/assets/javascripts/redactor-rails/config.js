0caed2264d4bf733c02fba889ee12c306b966e0d
Arturs Braucs <arturs.braucs@gmail.com>
2015-01-20 09:01:39 +0200

    Change gem name, add file upload support by default

--- vendor/assets/javascripts/redactor-rails/config.js
+++ vendor/assets/javascripts/redactor-rails/config.js
@@ -6,11 +6,13 @@ window.init_redactor = function(){
     params = csrf_param + "=" + encodeURIComponent(csrf_token);
   }
   $('.redactor').redactor(
-    { "imageUpload":"/redactor_rails/pictures?" + params,
-      "imageGetJson":"/redactor_rails/pictures",
-      "path":"/assets/redactor-rails",
-      "css":"style.css"}
-  );
+      { "imageUpload":"/redactor_rails/pictures?" + params,
+        "imageGetJson":"/redactor_rails/pictures",
+        "fileUpload":"/redactor_rails/documents?" + params,
+        "fileGetJson":"/redactor_rails/documents",
+        "path":"/assets/redactor-rails",
+        "css":"style.css"}
+      );
 }
 
 $(document).ready( window.init_redactor );
