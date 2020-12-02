51de1d47cac942150fbdf90a42c817adb777313c
mateo9 <mvukusic199@gmail.com>
2020-12-02 15:03:03 +0100

    Change size to length

--- vendor/assets/javascripts/redactor-rails/redactor.js
+++ vendor/assets/javascripts/redactor-rails/redactor.js
@@ -1140,7 +1140,7 @@
 			// blur callback
 			this.$editor.on('blur.redactor', $.proxy(function(e)
 			{
-				if (!$(clickedElement).hasClass('redactor_toolbar') && $(clickedElement).parents('.redactor_toolbar').size() == 0)
+				if (!$(clickedElement).hasClass('redactor_toolbar') && $(clickedElement).parents('.redactor_toolbar').length() == 0)
 				{
 					this.selectall = false;
 					if ($.isFunction(this.opts.blurCallback)) this.callback('blur', e);
@@ -2425,7 +2425,7 @@
 			var btn = this.buttonBuild(key, { title: title, callback: callback, dropdown: dropdown });
 			var $btn = this.buttonGet(afterkey);
 
-			if ($btn.size() !== 0) $btn.parent().after($('<li>').append(btn));
+			if ($btn.length() !== 0) $btn.parent().after($('<li>').append(btn));
 			else this.$toolbar.append($('<li>').append(btn));
 		},
 		buttonAddBefore: function(beforekey, key, title, callback, dropdown)
@@ -2434,7 +2434,7 @@
 			var btn = this.buttonBuild(key, { title: title, callback: callback, dropdown: dropdown });
 			var $btn = this.buttonGet(beforekey);
 
-			if ($btn.size() !== 0) $btn.parent().before($('<li>').append(btn));
+			if ($btn.length() !== 0) $btn.parent().before($('<li>').append(btn));
 			else this.$toolbar.append($('<li>').append(btn));
 		},
 		buttonRemove: function (key, separator)
@@ -2744,10 +2744,10 @@
 						if (s.tagName == 'LI')
 						{
 							var $prev = $(s).prev();
-							if ($prev.size() != 0 && $prev[0].tagName == 'LI')
+							if ($prev.length() != 0 && $prev[0].tagName == 'LI')
 							{
 								var $childList = $prev.children('ul, ol');
-								if ($childList.size() == 0)
+								if ($childList.length() == 0)
 								{
 									$prev.append($('<' + listTag + '>').append(s));
 								}
@@ -2861,7 +2861,7 @@
 			if (li && li.tagName == 'LI')
 			{
 				var $parent = $(li).parent().parent();
-				if ($parent.size() != 0 && $parent[0].tagName == 'LI')
+				if ($parent.length() != 0 && $parent[0].tagName == 'LI')
 				{
 					$parent.after(li);
 				}
@@ -3514,7 +3514,7 @@
 					if (tag === 'p')
 					{
 						if ((node.tagName === 'P'
-						&& parent.size() != 0
+						&& parent.length() != 0
 						&& parent[0].tagName === 'BLOCKQUOTE')
 						||
 						node.tagName === 'BLOCKQUOTE')
@@ -3632,7 +3632,7 @@
 							// from blockquote
 							if (blockquote[0].tagName == 'BLOCKQUOTE')
 							{
-								var count = $(blockquote).children('p').size();
+								var count = $(blockquote).children('p').length();
 
 								// one
 								if (count == 1)
@@ -3767,7 +3767,7 @@
 					$(wrapper).html(html);
 					this.selectionElement(wrapper);
 					var next = $(wrapper).next();
-					if (next.size() != 0 && next[0].tagName === 'BR')
+					if (next.length() != 0 && next[0].tagName === 'BR')
 					{
 						next.remove();
 					}
@@ -4952,7 +4952,7 @@
 
 			$.each(nodes, $.proxy(function(i,node)
 			{
-				if (this.opts.iframe === false && $(node).parents('div.redactor_editor').size() == 0) return false;
+				if (this.opts.iframe === false && $(node).parents('div.redactor_editor').length() == 0) return false;
 				if (this.nodeTestBlocks(node)) newnodes.push(node);
 
 			}, this));
@@ -4993,7 +4993,7 @@
 
 			$.each(nodes, $.proxy(function(i,node)
 			{
-				if (this.opts.iframe === false && $(node).parents('div.redactor_editor').size() == 0) return false;
+				if (this.opts.iframe === false && $(node).parents('div.redactor_editor').length() == 0) return false;
 
 				if (typeof tag === 'undefined')
 				{
@@ -5290,7 +5290,7 @@
 		{
 			var $table = $(this.getParent()).closest('table');
 			if (!this.isParentRedactor($table)) return false;
-			if ($table.size() == 0) return false;
+			if ($table.length() == 0) return false;
 
 			this.bufferSet();
 
@@ -5301,7 +5301,7 @@
 		{
 			var $table = $(this.getParent()).closest('table');
 			if (!this.isParentRedactor($table)) return false;
-			if ($table.size() == 0) return false;
+			if ($table.length() == 0) return false;
 
 			this.bufferSet();
 
@@ -5324,7 +5324,7 @@
 		{
 			var $table = $(this.getParent()).closest('table');
 			if (!this.isParentRedactor($table)) return false;
-			if ($table.size() == 0) return false;
+			if ($table.length() == 0) return false;
 
 			this.bufferSet();
 
@@ -5351,11 +5351,11 @@
 		{
 			var $table = $(this.getParent()).closest('table');
 			if (!this.isParentRedactor($table)) return false;
-			if ($table.size() == 0) return false;
+			if ($table.length() == 0) return false;
 
 			this.bufferSet();
 
-			if ($table.find('thead').size() !== 0) this.tableDeleteHead();
+			if ($table.find('thead').length() !== 0) this.tableDeleteHead();
 			else
 			{
 				var tr = $table.find('tr').first().clone();
@@ -5373,7 +5373,7 @@
 			if (!this.isParentRedactor($table)) return false;
 			var $thead = $table.find('thead');
 
-			if ($thead.size() == 0) return false;
+			if ($thead.length() == 0) return false;
 
 			this.bufferSet();
 
@@ -5400,7 +5400,7 @@
 		{
 			var $table = $(this.getParent()).closest('table');
 			if (!this.isParentRedactor($table)) return false;
-			if ($table.size() == 0) return false;
+			if ($table.length() == 0) return false;
 
 			this.bufferSet();
 
@@ -5417,7 +5417,7 @@
 		{
 			var $table = $(this.getParent()).closest('table');
 			if (!this.isParentRedactor($table)) return false;
-			if ($table.size() == 0) return false;
+			if ($table.length() == 0) return false;
 
 			this.bufferSet();
 
@@ -5719,7 +5719,7 @@
 				this.execCommand('inserthtml', link, false);
 
 				var linkmarker = $(this.$editor.find('a#filelink-marker'));
-				if (linkmarker.size() != 0) linkmarker.removeAttr('id');
+				if (linkmarker.length() != 0) linkmarker.removeAttr('id');
 				else linkmarker = false;
 
 				this.sync();
@@ -5959,7 +5959,7 @@
 			{
 				this.imageMargin = '0px';
 				var imageBox = $el.closest('#redactor-image-box');
-				if (imageBox.size() != 0) imageBox.css({ 'float': '', 'margin': '' });
+				if (imageBox.length() != 0) imageBox.css({ 'float': '', 'margin': '' });
 				$el.css({ 'float': '', 'margin': '' });
 			}
 
@@ -6012,13 +6012,13 @@
 		},
 		imageResizeHide: function(e)
 		{
-			if (e !== false && $(e.target).parent().size() != 0 && $(e.target).parent()[0].id === 'redactor-image-box')
+			if (e !== false && $(e.target).parent().length() != 0 && $(e.target).parent()[0].id === 'redactor-image-box')
 			{
 				return false;
 			}
 
 			var imageBox = this.$editor.find('#redactor-image-box');
-			if (imageBox.size() == 0)
+			if (imageBox.length() == 0)
 			{
 				return false;
 			}
@@ -6072,7 +6072,7 @@
 
 			$image.on('click', $.proxy(function(e)
 			{
-				if (this.$editor.find('#redactor-image-box').size() != 0)
+				if (this.$editor.find('#redactor-image-box').length() != 0)
 				{
 					return false;
 				}
@@ -6236,7 +6236,7 @@
 			var img = '<img id="image-marker" src="' + $(e.target).attr('rel') + '" alt="' + $(e.target).attr('title') + '" />';
 
 			var parent = this.getParent();
-			if (this.opts.paragraphy && $(parent).closest('li').size() == 0) img = '<p>' + img + '</p>';
+			if (this.opts.paragraphy && $(parent).closest('li').length() == 0) img = '<p>' + img + '</p>';
 
 			this.imageInsert(img, true);
 		},
@@ -6270,7 +6270,7 @@
 					html = '<img id="image-marker" src="' + json.filelink + '" />';
 
 					var parent = this.getParent();
-					if (this.opts.paragraphy && $(parent).closest('li').size() == 0)
+					if (this.opts.paragraphy && $(parent).closest('li').length() == 0)
 					{
 						html = '<p>' + html + '</p>';
 					}
